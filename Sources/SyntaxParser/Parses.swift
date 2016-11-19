31e9ac11a45db3e56d2b15df9990a8b7de8566d2
Nanjiang Jiang <njiang8@illinois.edu>
2016-11-19 16:09:14 -0600

    syntax parserrrrr

---
+++ Sources/SyntaxParser/Parses.swift
@@ -0,0 +1,16 @@
+//
+//  Parses.swift
+//  EasierCCG
+//
+//  Created by Chelsea Jiang on 11/19/16.
+//
+//
+
+import CCG
+
+public struct Parses {
+    let headWord: String
+    let headIndex: Int
+    let score: Double
+    let cat: SyntaxTree
+}
